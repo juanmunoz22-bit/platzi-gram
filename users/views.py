ceeef0c88aadfcd701bc1b3d5d209b81d15d2de8
Your Name <you@example.com>
2021-12-22 21:31:45 -0500

    logout functionalities

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 # Django
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 def login_view(request):
     if request.method == 'POST':
@@ -13,3 +14,8 @@ def login_view(request):
         else:
             return render(request, 'users/login.html', {'error': 'Invalid username and password'})
     return render(request, 'users/login.html')
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
