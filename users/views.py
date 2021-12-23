54258619cf8613b1aebc0f71b78839569160234d
Your Name <you@example.com>
2021-12-22 22:03:34 -0500

    signup functionalities

--- users/views.py
+++ users/views.py
@@ -3,6 +3,43 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 
+#Exceptions
+from django.db.utils import IntegrityError
+
+# Models
+
+    #User
+from django.contrib.auth.models import User
+
+    #Profile
+from users.models import Profile 
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        pass_confirm = request.POST['password_confirmation']
+
+        if password != pass_confirm:
+            return render(request, 'users/signup.html', {'error': 'Passwords does not match'})
+
+        try:
+            user: User = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username already exists'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile: Profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
+
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
