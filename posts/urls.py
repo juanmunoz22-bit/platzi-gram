f44192384db70307f3bf21bc75fecca33bb06f7e
Your Name <you@example.com>
2021-12-23 22:36:37 -0500

    First approach to Class Based Views and reordering the urls config

---
+++ posts/urls.py
@@ -0,0 +1,19 @@
+# Django
+from django.urls import path
+
+# Posts
+from posts import views
+
+urlpatterns = [
+    path(
+        route='', 
+        view=views.list_posts,
+        name='feed'
+    ),
+    
+    path(
+        route='posts/new/', 
+        view=views.create_post, 
+        name="create"
+    ),
+]
